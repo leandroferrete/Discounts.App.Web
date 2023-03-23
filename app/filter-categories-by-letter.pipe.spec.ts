be645ac66906b8844028cc908480c00a7de2f502
LeandroFerrete <leandro.ferrete@hotmail.com>
2023-03-23 00:54:08 -0300

    no message

--- app/filter-categories-by-letter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterCategoriesByLetterPipe } from './filter-categories-by-letter.pipe';
-
-describe('FilterCategoriesByLetterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterCategoriesByLetterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- app/filter-stores-by-letter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterStoresByLetterPipe } from './filter-stores-by-letter.pipe';
-
-describe('FilterStoresByLetterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterStoresByLetterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
